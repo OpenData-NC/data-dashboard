4b99108876c7cf7cada8715746b190a331975b92
Vaughn Hagerty <vaughn.hagerty@gmail.com>
2014-09-09 18:20:06 -0400

    updated port number message in app.js

--- api/app.js
+++ api/app.js
@@ -6,24 +6,11 @@ var express = require('express')
 MongoClient.connect('mongodb://localhost:27017/crime', function(err, db) {
     "use strict";
     if(err) throw err;
-//    db.collection('incidents').findOne( {county: 'New Hanover'}, function(err, doc){
-//        console.log(doc);
-//    });    // Register our templating engine
-//    app.engine('html', cons.swig);
-//    app.set('view engine', 'html');
-//    app.set('views', __dirname + '/views');
-
-    // Express middleware to populate 'req.cookies' so we can access cookies
-//    app.use(express.cookieParser());
-
-    // Express middleware to populate 'req.body' so we can access POST variables
-//    app.use(express.bodyParser());
-
     // Application routes
     routes(app, db);
 
 
     app.listen(3000);
-    console.log('Express server listening on port 8082');
+    console.log('Express server listening on port 3000');
 });
 
