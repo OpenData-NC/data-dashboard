b01074746fd9cb63c70e1291ca07eb8727f00dd3
Vaughn Hagerty <vaughn.hagerty@gmail.com>
2015-02-26 12:37:37 -0500

    updated flask app, js, css, html for dashboard

--- dashboard/search.py
+++ dashboard/search.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
 from titlecase import titlecase
 import MySQLdb
-
+import datetime
 
 app = Flask(__name__)
 
@@ -10,9 +10,9 @@ user = 'dataDa5h'
 pw = 'UnC0p3n'
 db = 'crime'
 host = '10.240.220.181'
-connection = MySQLdb.connect(user=user, passwd=pw, db=db, host=host)
+#connection = MySQLdb.connect(user=user, passwd=pw, db=db, host=host)
 #connection = MySQLdb.connect(user=user, passwd=pw, db=db)
-cursor = connection.cursor()
+#cursor = connection.cursor()
 
 #split the query into a dictionary with the field as key and search string as value
 def make_params(query):
@@ -36,17 +36,20 @@ def build_query(data_type, params):
                       'county': 'county = "%s"', 'from-date': 'reported_date = "%s"',
                       'to-date': '(reported_date >= "%s" and reported_date <= "%s")',
                       'street-number': 'address like "%s %%"',
-                      'street-name': 'address like "%%%s%%"'},
+                      'street-name': 'address like "%%%s%%"', 'category-type': 'category in (%s)', 
+                      'gender-type': 'sex in (%s)', 'min-age': 'age >= %s', 'max-age': 'age <= %s'},
         'arrests': {'first-name': 'name like "%% %s%%"', 'last-name': 'name like "%s,%%"',
                       'county': 'county = "%s"', 'from-date': 'date_occurred = "%s"',
                       'to-date': '(date_occurred >= "%s" and date_occurred <= "%s")',
                       'street-number': 'address like "%s %%"',
-                      'street-name': 'address like "%%%s%%"'},
+                      'street-name': 'address like "%%%s%%"', 'category-type': 'category in (%s)', 
+                      'gender-type': 'sex in (%s)', 'min-age': 'age >= %s', 'max-age': 'age <= %s'},
         'citations': {'first-name': 'name like "%% %s%%"', 'last-name': 'name like "%s,%%"',
                       'county': 'county = "%s"', 'from-date': 'date_occurred = "%s"',
                       'to-date': '(date_occurred >= "%s" and date_occurred <= "%s")',
                       'street-number': 'address like "%s %%"',
-                      'street-name': 'address like "%%%s%%"'},
+                      'street-name': 'address like "%%%s%%"', 
+                      'gender-type': 'sex in (%s)', 'min-age': 'age >= %s', 'max-age': 'age <= %s'},
         'accidents': {'first-name': '(name1 like "%s%%" or name2 like "%s%%")',
                       'last-name': '(name1 like "%% %s" or name2 like "%% %s")',
                       'county': 'county = "%s"', 'from-date': 'date_occurred = "%s"',
@@ -57,57 +60,64 @@ def build_query(data_type, params):
                       'last-name': 'last_name = "%s"',
                       'county': 'county_desc = "%s"',
                       'street-number': 'res_street_address like "%s %%"',
-                      'street-name': 'res_street_address like "%%%s%%"'},
+                      'street-name': 'res_street_address like "%%%s%%"', 'city': 'res_city_desc = "%s"',
+                      'party-type': 'party_cd in (%s)', 'gender-type': 'gender_code in (%s)'},
         'rr': {'first-name': 'fac_name like "%%%s%%"', 'last-name': 'fac_name like "%s%%"',
-                      'county': 'county = "%s"', 'from-date': 'activity_date = "%s"',
+                      'location-name': 'fac_name like "%%%s%%"', 'county': 'county = "%s"', 'from-date': 'activity_date = "%s"',
                       'to-date': '(activity_date >= "%s" and activity_date <= "%s")',
                       'street-number': 'addr_line1 like "%s %%"',
-                      'street-name': 'addr_line1 like "%%%s%%"'},
-        'dash_buncombe_property': {'first-name': '(owner1_firstname = "%s" or owner2_firstname = "%s")',
+                      'street-name': 'addr_line1 like "%%%s%%"', 'city': 'addr_city = "%s"', 'zip-code': 'addr_zip5 = "%s"'},
+        'dash_buncombe_property': {'first-name': '(owner1_firstname like "%s%%" or owner2_firstname like "%s%%")',
                       'last-name': '(owner1_lastname = "%s" or owner2_lastname = "%s")',
                       'from-date': 'deeddate = "%s"',
                       'to-date': '(deeddate >= "%s" and deeddate <= "%s")',
                       'street-number': 'housenum = "%s"',
-                      'street-name': 'streetname = "%s"'},
-        'dash_buncombe_real_estate': {'first-name': '(seller1_fname = "%s" or seller2_fname = "%s" or buyer1_fname = "%s" or buyer2_fname = "%s")',
+                      'street-name': 'streetname = "%s"', 'min-value': 'taxableVal >= %s', 'max-value': 'taxableVal <= %s'},
+        'dash_buncombe_real_estate': {'first-name': '(seller1_fname like "%s%%" or seller2_fname like "%s%%" or buyer1_fname like "%s%%" or buyer2_fname like "%s%%")',
                       'last-name': '(seller1_lname = "%s" or seller2_lname = "%s" or buyer1_lname = "%s" or buyer2_lname = "%s")',
+                      'buyer-first-name': '(buyer1_fname like "%s%%" or buyer2_fname like "%s%%")', 'buyer-last-name': '(buyer1_lname = "%s" or buyer2_lname = "%s")',
+                      'seller-first-name': '(seller1_fname like "%s%%" or seller2_fname like "%s%%")', 'seller-last-name': '(seller1_lname = "%s" or seller2_lname = "%s")',                      
                       'from-date': 'selldate = "%s"',
                       'to-date': '(selldate >= "%s" and selldate <= "%s")',
                       'street-number': 'housenum = "%s"',
-                      'street-name': 'streetname = "%s"'},
+                      'street-name': 'streetname = "%s"','min-value': 'sellingPrice >= %s', 'max-value': 'sellingPrice <= %s'},
 
-        'dash_nh_property': {'first-name': '(owner like "%s%%" or `co-owner` like "%s%%")',
-                      'last-name': '(owner like "%% %s" or `co-owner` like "%% %s")',
-                      'street-number': '`situs-num` = "%s"',
-                      'street-name': '`situs-street` = "%s"'},
+        'dash_nh_property': {'first-name': '(owner like "%%%s%%" or `co-owner` like "%%%s%%")',
+                      'last-name': '(owner like "%s%%" or `co-owner` like "%s%%")',
+                      'street-number': '`situs-num` like "%%%s"',
+                      'street-name': '`situs-street` like "%s%%"', 'min-value': '`real-assval` >= %s', 'max-value': '`reall-assval` <= %s'},
 
-        'dash_nh_real_estate': {'first-name': '(seller like "%s%%" or seller like "%s%%")',
-                      'last-name': '(buyer like "%% %s" or buyer like "%% %s")',
+        'dash_nh_real_estate': {'first-name': '(seller like "%s%%" or buyer like "%s%%")',
+                      'last-name': '(seller like "%% %s" or buyer like "%% %s")',
+                      'buyer-first-name': 'buyer like "%s%%"', 'buyer-last-name': 'buyer like "%% %s"',
+                      'seller-first-name': 'seller like "%s%%"', 'seller-last-name': 'seller like "%% %s"',                      
                       'from-date': 'sale_date = "%s"',
                       'to-date': '(sale_date >= "%s" and sale_date <= "%s")',
                       'street-number': 'address like "%s %%"',
-                      'street-name': 'address like "%%%s%%"'},
+                      'street-name': 'address like "%%%s%%"', 'min-value': 'price >= %s', 'max-value': 'price <= %s'},
 
         'dash_wake_property': {'first-name': '(owner_line1 like "%s%%" or owner_line2 like "%s%%")',
                       'last-name': '(owner_line1 like "%% %s" or owner_line1 like "%% %s")',
                       'from-date': 'total_sale_date = "%s"',
                       'to-date': '(total_sale_date >= "%s" and total_sale_date <= "%s")',
                       'street-number': 'site_address_street_number = "%s"',
-                      'street-name': 'site_address_street_name = "%s"'},
+                      'street-name': 'site_address_street_name = "%s"', 'min-value': 'sum(building_assessed_value,land_assessed_value) >= %s', 'max-value': 'sum(building_assessed_value,land_assessed_value) <= %s'},
 
         'dash_wake_real_estate': {'first-name': '(buyer_line1 like "%%%s%%" or buyer_line2 like "%%%s%%")',
                       'last-name': '(buyer_line1 like "%%%s%%" or buyer_line2 like "%%%s%%")',
+                      'buyer-first-name': '(buyer_line1 like "%%%s%%" or buyer_line2 like "%%%s%%")', 'buyer-last-name': '(buyer_line1 like "%%%s%%" or buyer_line2 like "%%%s%%")',
+                      'seller-first-name': '', 'seller-last-name': '',     #no seller data in wake                 
                       'from-date': 'total_sale_date = "%s"',
                       'to-date': '(total_sale_date >= "%s" and total_sale_date <= "%s")',
                       'street-number': 'site_address_street_number = " %s"',
-                      'street-name': 'site_address_street_name = " %s"'}
+                      'street-name': 'site_address_street_name = " %s"', 'min-value': 'total_sale_price >= %s', 'max-value': 'total_sale_price <= %s'}
     }
 #fields we're selecting for return for each table
     selects = {
-        'incidents': 'record_id `Record ID`, agency `Agency`, name `Name`, date_format(date_reported,"%%m/%%d/%%Y") `Date reported` , category `Category`, incidents.charge `Charge`, address `Address`',
-        'arrests': 'record_id `Record ID`, agency `Agency`, name `Name`, date_format(date_occurred,"%%m/%%d/%%Y") `Date occurred`, `Category`, arrests.charge `Charge`, address `Address`',
+        'incidents': 'record_id `Record ID`, agency `Agency`, name `Name`, date_format(date_reported,"%%m/%%d/%%Y") `Date reported` , category `Category`, incidents.charge `Charge`, address `Address`, if(pdf="" or pdf is null, "N/A", pdf) `View report`',
+        'arrests': 'record_id `Record ID`, agency `Agency`, name `Name`, date_format(date_occurred,"%%m/%%d/%%Y") `Date occurred`, `Category`, arrests.charge `Charge`, address `Address`, if(pdf="" or pdf is null, "N/A", pdf) `View report`',
         'citations': 'record_id `Record ID`, agency `Agency`, name `Name`, date_format(date_occurred,"%%m/%%d/%%Y") `Date occurred`, charge, address `Address`',
-        'accidents': 'record_id `Record ID`, agency `Agency`, name1 `Driver 1`, name2 `Driver 2`, date_format(date_occurred,"%%m/%%d/%%Y") `Date occurred`, address `Address`',
+        'accidents': 'record_id `Record ID`, agency `Agency`, name1 `Driver 1`, name2 `Driver 2`, date_format(date_occurred,"%%m/%%d/%%Y") `Date occurred`, address `Address`, if(pdf="" or pdf is null, "N/A", pdf) `View report`',
         'nc_voters_new': 'voter_reg_num `Voter reg. num.`, first_name `First name`, last_name `Last name`, res_street_address `Address` , res_city_desc `City`, zip_code `Zip code`',
         'rr': 'fac_name `Facility name`, addr_line1 `Address`, addr_city `City`, addr_zip5 `ZIP code`,date_format(activity_date,"%%m/%%d/%%Y") `Insp. date`, activity_final_score `Score`',
         'dash_buncombe_property': 'parcelid `Parcel ID`, concat_ws(" ", owner1_firstname, owner1_lastname) `Owner 1`, concat_ws(" ", owner2_firstname, owner2_lastname) `Owner 2`, date_format(deeddate,"%%m/%%d/%%Y") `Deed date`, concat_ws(" ", housenum, housesuffix, streetdirection, streetname, streettype) `address`, citycode `City code`, taxableval `Tax value`',
@@ -146,11 +156,20 @@ def build_query(data_type, params):
     name_counts = {'accidents': 1, 'dash_buncombe_property': 1, 'dash_buncombe_real_estate': 3,'dash_nh_property': 1, 'dash_nh_real_estate': 1,'dash_wake_property': 1, 'dash_wake_real_estate': 1 }
 #we don't use the county in these tables. they don't have a county field because they're already county-specific.
     skip_counties = ['dash_buncombe_property','dash_buncombe_real_estate','dash_nh_property', 'dash_nh_real_estate','dash_wake_property', 'dash_wake_real_estate']
+    #no seller data in wake
+    skip_sellers = ['dash_wake_real_estate']
+    #the query for these needs to use in (val1, val2)
+    in_categories = ['category-type', 'party-type', 'gender-type']
 #we'll use these to build our query
     query_string = []
     query_vals = []
 
     for param in params:
+        if data_type in skip_sellers and (param == 'seller-first-name' or param == 'seller-last-name'):
+            continue;
+        if param in in_categories:
+            query_string.append(search_items[param])
+            query_vals.append(format_categories(params[param]))
         if param == 'from-date' and 'to-date' not in params and data_type not in skip_dates:
             query_string.append(search_items['from-date'])
             query_vals.append(params[param])
@@ -158,7 +177,7 @@ def build_query(data_type, params):
             query_string.append(search_items['to-date'])
             query_vals.append(params['from-date'])
             query_vals.append(params['to-date'])
-        if param not in skip_params:
+        if param not in skip_params and param not in in_categories:
             if param == 'county' and data_type in skip_counties:
                 continue;
             query_string.append(search_items[param])
@@ -181,6 +200,9 @@ def build_query(data_type, params):
 #we try to encode it, but failing that for now we don't return it.
 #we'll need to try to address this later.
 
+def format_categories(cats):
+    return '"' + cats.replace('-',',"') + '"'
+
 def clean_data(item):
     try:
         cleaned_item = str(item).encode('utf-8')
@@ -191,12 +213,18 @@ def clean_data(item):
 #execute our query and return the data    
 def query_db(sql, query_vals):
 #we're currently limiting the return to 1000 rows
+    connection = MySQLdb.connect(user=user, passwd=pw, db=db, host=host)
+#connection = MySQLdb.connect(user=user, passwd=pw, db=db)
+    cursor = connection.cursor()
+    sql_return = sql % query_vals
 #    cursor.execute(sql + ' limit 1000')
     try:
         cursor.execute(sql % query_vals)
     except:
     #for now. should return error
-        return {'headings':[], 'data': []}
+        log(sql_return, 'Failed query')
+        connection.close()
+        return {'headings':[], 'data': [], 'sql': sql_return}
     formatted_data = []
 #we'll need the field names for headings in the json
     cols = [ d[0] for d in cursor.description ]
@@ -204,17 +232,18 @@ def query_db(sql, query_vals):
     for row in data:
         cleaned_row = [clean_data(item) for item in row]
         formatted_data.append(cleaned_row)
-    return { 'headings': cols, 'data': formatted_data }
+    connection.close()
+    return { 'headings': cols, 'data': formatted_data , 'sql': sql_return}
 
 
 #our very simplistic routing
 #right now, we're just grabbing everything after dashboard/
 #which is the home directory of the app
 
-@app.route('/<query>')
+@app.route('/<query>',methods = ['GET'])
 def make_index(query=None):
 #this will hold the data we're returning
-
+    log(query)
     data = {}
     params = make_params(query)
 #map the data type requested to the actual table name
@@ -226,6 +255,10 @@ def make_index(query=None):
                 'realestate-New Hanover': 'dash_nh_real_estate',
                 'property-Wake': 'dash_buncombe_property',
                 'realestate-Wake': 'dash_wake_real_estate',
+                'incidents': 'incidents',
+                'arrests': 'arrests',
+                'accidents': 'accidents',
+                'citations': 'citations',
             }
 #build the table names used for the queries
 #for each data type requested, query and format the results
@@ -241,13 +274,17 @@ def make_index(query=None):
 #use a combination of the county and the requested data type to figure out
 #which table to use for property and real estate
             if data_type == 'property' or data_type == 'realestate':
-                table_key = '-'.join([data_type,params['county']])
+                table_key = '-'.join([data_type,titlecase(params['county'])])
             else:
                 table_key = data_type
             data[data_type] = build_query(tables[table_key], params)
 #return the json object
     return jsonify(results=data)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 #not currently used, but a utility to try to format all-caps data
 #because we can't always predict what should or shouldn't be capped
 #we compromise by making it titlecase
@@ -261,6 +298,13 @@ def up(row):
             uppered.append(item)
     return uppered
 
-#we have debugging on for now    
+#for debugging
+def log(msg, type="query"):
+    with open('/var/www/log/wsgi.log', 'a') as l:
+        d = datetime.datetime.now()
+        l.write("\t".join([msg,type, str(d)]) + "\n")
+        l.close()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
+
--- dashboard/stats/html/lib/js/dd-search.js
+++ dashboard/stats/html/lib/js/dd-search.js
@@ -1,64 +1,91 @@
 ﻿    (function(){
+        var county_dir = find_county();
+        var county = dd_config[county_dir].county;
+        $('#dd-county').text(county);
+//load the form html and add click event to search button
+        $('#dd-form').load('form.html', add_click);
+        $('.dd-nav').each(function() {
+            $(this).attr('href', '/' + county_dir + '/' + $(this).attr('href'));
+        });
+        
         google.load("visualization", "1", {packages:["table"]});
-        $('#search').click(function(){
-            query = find_query_params();
-            query?query_data(query):error();
-            
-            function query_data(query) {
-                $('#data-tables').empty();
-                $('#data-tables').html('<h2>Searching ...</h2>');
-                $.get(query)
-                    .done(function(data){
-                        console.log(data);
-			show_data(data);
-                    });
+        function add_click() {
+            $('#search').click(function(){
+                var query = find_query_params();
+                query?query_data(query):error();
+            });
+        }
+        function query_data(query) {
+            $('#data-tables').empty();
+            $('#data-tables').html('<h2>Searching ...</h2>');
+            $.get(query)
+                .done(function(data){
+                    console.log(data);
+                    show_data(data);
+                });
 
-            }
-            function error(){
-                $('#data-tables').html('<h4 class="text-danger">WTF?? Specify at least one search category.</h4>');
-                
-            }
-            function find_query_params() {
-                var search_params = {}
-                    , data_types = [];
-                $('.search-item').each(function(i,item){
-                    if($(item).val() !== '') {
-                    search_params[$(item).prop('id')] = $(item).val();
+        }
+        function error(){
+            $('#data-tables').html('<h4 class="text-danger">Specify at least one search category.</h4>');
+            
+        }
+        function find_query_params() {
+            //these need to be formatted to allow multiple values
+            var multiples = ['category-type','party-type', 'gender-type'];
+            var search_params = {}
+                , data_types = [];
+            $('.search-item').each(function(i,item){
+                if($(item).val() !== '') {
+                search_params[$(item).prop('id')] = $(item).val();
+                }
+            });
+            //here's where we format the multiple values
+            multiples.forEach(function(multiple_item) {
+                var multiple_vals = [];
+                $('.' + multiple_item).each(function(i,item){
+                    if($(item).is(':checked') || $(item).val() === "picked") {
+                        multiple_vals.push($(item).prop('id'));
                     }
                 });
-                $('.data-type').each(function(i,item){
-//                $('#crime, #voter').each(function(i,item){
-                    if($(item).is(':checked')) {
-                        data_types.push($(item).prop('id'));
-                    }
+                if(multiple_vals.length > 0) {
+                    search_params[multiple_item] = multiple_vals.join('-');
+                }
+            });
+            $('.data-type').each(function(i,item){
+                if($(item).is(':checked') || $(item).val() === "picked") {
+                    data_types.push($(item).prop('id'));
+                }
                 search_params['data_types'] = data_types.join('-');
-                });
-                return data_types.length === 0?false:build_query(search_params);
-            }
-            function build_query(search_params) {
-                var county = get_county();
-//                console.log(county);
-                var query = ['county', county]
-                    ,base = '/search';
-                $.each(search_params, function(key, value) {
-                    query.push(key);
-                    query.push(value);
-                });
-                return [base, query.join('|')].join('/');
-                
-            }
-            function show_data(data){
+            });
+
+            
+            return data_types.length === 0?false:build_query(search_params);
+//            return has_data_type?build_query(search_params):false;
+        }
+        
+        function build_query(search_params) {
+            var query = ['county', county]
+                ,base = '/search';
+            $.each(search_params, function(key, value) {
+                query.push(key);
+                query.push(value);
+            });
+            return [base, query.join('|')].join('/');
+            
+        }
+        function show_data(data){
 //		console.log(data);
-                $('#data-tables').empty();
-                for (data_type in data.results) {
+            $('#data-tables').empty();
+            for (data_type in data.results) {
 //                    console.log(data_type);
 //                    console.log(data.results[data_type]);
-                    if(data.results[data_type].data.length > 0) {
-                        build_table(data_type, data.results[data_type]);
-                    }
+                if(data.results[data_type].data.length > 0) {
+                    build_table(data_type, data.results[data_type]);
                 }
-            };
-        });
+            }
+        }
+     
+//not used anymore
         function get_county(){
             var county = '';
             $('.county-location').each(function(i,item){
@@ -69,9 +96,23 @@
             if(county === '') { county = 'New Hanover'; }
             return county;
         }
+//uses url ad config file to get county        
+        function find_county() {
+            var county_re = /org\/([^\/]+)\//;
+            var county_match = county_re.exec(location.href);
+            return county_match[1];        
+        }
+        
         function build_table(data_type, data_content, page_size){
             if(data_type === 'arrests' || data_type === 'incidents' || data_type === 'accidents' || data_type === 'citations') {
-                formatted_table_data = data_content.data.map(function(row) { row[0] = truncate(row[0], 15); return row; });
+                formatted_table_data = data_content.data.map(function(row) { 
+                    row[0] = truncate(row[0], 15);
+                    if(row[row.length-1].indexOf('pdf') !== -1) {
+                        var pdf_url = row[row.length-1].replace('/home/vaughn.hagerty/crime-scrapers','');
+                        row[row.length-1] = '<a href="http://130.211.132.6' + pdf_url + '" target="_blank">Report</a>';
+                    }
+                    return row; 
+                });
             }
             else { formatted_table_data = data_content.data; }
             var rows = $('.row');
@@ -82,7 +123,7 @@
                 data.addColumn("string",heading);
             });
             data.addRows(formatted_table_data);
-            var options = {page:"enable",pageSize:20};
+            var options = {page:'enable',pageSize:20, allowHtml: true};
             var table_div_id = 'data-table-' + data_type;
             var table_div = '<h2 style="text-transform: capitalize">' + data_type + ': ' + data_content.data.length + ' records</h2><div style="width:100%" id="' + table_div_id + '"></div>';
             $('#data-tables').append(table_div);
